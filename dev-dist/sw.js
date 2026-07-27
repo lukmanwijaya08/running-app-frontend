1a6808e7910d1fb17f1ea0128ba23a0df159e0df
lukmanwijaya08 <lukmanwijayanto39@gmail.com>
2026-07-27 21:03:02 +0700

    penambahan fitur share image

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.td1np1b0n6"
+    "revision": "0.vbj8glijl2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
